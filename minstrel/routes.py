ec659765167341c9a0736d3f3448c5c4a76e0ccc
Eli Ribble <eli@authentise.com>
2017-09-13 14:56:54 -0600

    Add the ability to list music and play it in the browser
    Simple, but still, it works

--- minstrel/routes.py
+++ minstrel/routes.py
@@ -1,7 +1,17 @@
 import flask
+import os
 
 blueprint = flask.Blueprint('routes', __name__)
 
 @blueprint.route('/')
 def index():
-    return flask.render_template('root.html')
+    music = [track for track in os.listdir('/music') if os.path.isfile('/music/' + track)]
+    return flask.render_template('root.html', music=music)
+
+@blueprint.route('/track/<filename>/')
+def track(filename):
+    return flask.render_template('track.html', filename=filename)
+
+@blueprint.route('/music/<path:path>/')
+def send_music(path):
+    return flask.send_from_directory('/music/', path)
